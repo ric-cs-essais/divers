5199ca3754aed399f650c30d3974a4ec42aecdf0
Richard PYT <rick.info.rsoc@gmail.com>
2021-02-07 18:20:34 +0100

    Ajout Test - Template class sur membre/méthode STATIC ! Ok, accepted in C# (contrairement à en Typescript).

---
+++ zzzz_POO_ClassTemplate_And_StaticMember_Autorisé_Contrairement_A_En_Typescript!/zzzzzTestsRapides/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json; //J'ai installé pour ça le Nugget NewonSoft.Json
+
+namespace zzzzzTestsRapides
+{
+
+    public class TestTemplateClassWithStatic<T>
+    {
+        public static List<T> myStaticMember; //Syntaxe acceptée
+
+        public static void myStaticMethod(List<T> param) { //Syntaxe acceptée
+            TestTemplateClassWithStatic<T>.myStaticMember = param;
+        }
+
+
+}
+
+//------------------------------------------------------------------------
+
+class Program
+    {
+        static void Main(string[] args)
+        {
+            TestTemplateClassWithStatic<int>.myStaticMethod(new List<int> {1,2,3});
+
+            string liste = JsonConvert.SerializeObject(TestTemplateClassWithStatic<int>.myStaticMember); //J'ai installé pour ça le Nugget NewonSoft.Json
+            Console.WriteLine(liste);
+
+            Console.WriteLine("OK");
+            Console.ReadKey();
+
+        }
+
+
+    }
+}
